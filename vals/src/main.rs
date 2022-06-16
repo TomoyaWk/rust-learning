5d40a68f7920beb52657d6970d9db88a501e68a6
tomoyaWk <wakatomo0606@gmail.com>
2022-06-16 09:57:27 +0900

    add creative-coding

--- vals/src/main.rs
+++ vals/src/main.rs
@@ -14,8 +14,31 @@ fn main() {
     myprint(&y2);// エラー回避方法②　あらかじめコピーしておく
     println!("{}", y2);
 
+
+    let mut s = "Hello!".to_string();
+    println!("s={}", s);
+
+
+    // myclear(&s); //関数に渡したリファレンス（＆s）が変更される場合はミュータブルなリファレンスを渡す(&mut hoge) 
+    let s_ref = &mut s;
+    myclear(s_ref);
+    println!("s={}", s);
+
+    // let h = return_hi(); //関数内のローカル変数へのリファレンスを返却値にすることはできない
+    // println!("{}", h);
+    
 }
 
 fn myprint<T: std::fmt::Display>(msg: T) {
     println!("in myprint: {}", msg);
+}
+
+fn myclear(x: &mut String){
+    x.clear();
+}
+
+
+fn return_hi() -> &String {
+    let s = "Hi!".to_string();
+    &s
 }
